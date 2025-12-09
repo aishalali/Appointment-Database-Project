e17f1121094a009e66c44e7bb81c8c16ff2972a9
shadowsoyaca <matthewkiy@gmail.com>
2025-12-08 22:02:02 -0600

    Edit Feature and Organizing Directories

---
+++ desktop-client/src/main/java/com/appointmentProject/desktop/controller/dashboard/NurseDashboardController.java
@@ -0,0 +1,66 @@
+/*************************************************************************
+ *  NurseDashboardController.java
+ *
+ *      This Class acts as the caller to the backend for specific data.
+ *      It will first connect each table column to a respective field in
+ *      a specified table. Using API client, it will talk to Spring Boot
+ *      backend. It will receive a response (in JSON format) and turn it
+ *      into a Java object. Afterwards it will display the obejcts into
+ *      a table for display.
+ *
+ * @author Matthew Kiyono
+ * @version 1.0
+ * @since 12/3/2025
+ **************************************************************************/
+package com.appointmentProject.desktop.controller.dashboard;
+/********************************************************************
+ *  NurseDashboardController.java
+ *
+ *          This controller provides a homepage for the Nurse user
+ *          type.
+ *
+ * @author Matthew Kiyono
+ * @version 1.1
+ * @since 12/3/2025
+ ********************************************************************/
+import com.appointmentProject.desktop.SceneNavigator;
+import com.appointmentProject.desktop.controller.ViewStaffController;
+import com.appointmentProject.desktop.controller.patient.ManagePatientController;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+public class NurseDashboardController {
+
+    @FXML
+    private Button backToLoginButton;
+
+    @FXML
+    private void initialize() {
+        // Runs automatically when FXML loads
+        System.out.println("Nurse Dashboard loaded.");
+    }
+
+    @FXML
+    public void handleManageAccount() {
+        AccountManagementController.setPreviousPage("/fxml/nurse_dashboard.fxml");
+        SceneNavigator.switchTo("/fxml/account_management.fxml");
+    }
+
+    @FXML
+    public void handleViewStaff() {
+        ViewStaffController.previousPage = "/fxml/nurse_dashboard.fxml";
+        SceneNavigator.switchTo("/fxml/view_staff.fxml");
+    }
+
+    @FXML
+    private void handleManagePatients(ActionEvent event) {
+        ManagePatientController.previousPage = "/fxml/nurse_dashboard.fxml";
+        SceneNavigator.switchTo("/fxml/manage_patient.fxml");
+    }
+
+    @FXML
+    private void handleLogout() {
+        SceneNavigator.switchTo("/fxml/login.fxml");
+    }
+}
---
+++ desktop-client/src/main/java/com/appointmentProject/desktop/controller/dashboard/ReceptionistDashboardController.java
@@ -0,0 +1,55 @@
+/*************************************************************************
+ *  ReceptionistDashboardController.java
+ *
+ *      This controller is responsible for providing the Receptionist user
+ *      type a "home" page.
+ *
+ * @author Matthew Kiyono
+ * @version 1.1
+ * @since 12/3/2025
+ **************************************************************************/
+package com.appointmentProject.desktop.controller.dashboard;
+
+import com.appointmentProject.desktop.SceneNavigator;
+import com.appointmentProject.desktop.controller.ViewStaffController;
+import com.appointmentProject.desktop.controller.emergencycontact.ManageContactsController;
+import com.appointmentProject.desktop.controller.patient.ManagePatientController;
+import javafx.fxml.FXML;
+
+public class ReceptionistDashboardController {
+
+    @FXML
+    private void initialize() {
+        System.out.println("Receptionist Dashboard Loaded.");
+    }
+
+    @FXML
+    public void handleManageAccount() {
+        AccountManagementController.setPreviousPage("/fxml/receptionist_dashboard.fxml");
+        SceneNavigator.switchTo("/fxml/account_management.fxml");
+    }
+
+    @FXML
+    public void handleViewStaff() {
+        ViewStaffController.previousPage = "/fxml/receptionist_dashboard.fxml";
+        SceneNavigator.switchTo("/fxml/view_staff.fxml");
+    }
+
+    @FXML
+    private void handleManagePatients() {
+        ManagePatientController.previousPage = "/fxml/receptionist_dashboard.fxml";
+        SceneNavigator.switchTo("/fxml/manage_patient.fxml");
+    }
+
+    @FXML
+    private void handleManageContacts() {
+        ManageContactsController.previousPage = "/fxml/receptionist_dashboard.fxml";
+        SceneNavigator.switchTo("/fxml/manage_contacts.fxml");
+    }
+
+
+    @FXML
+    private void handleLogout() {
+        SceneNavigator.switchTo("/fxml/login.fxml");
+    }
+}
